276b0f0eafab793809c3b235db8a3b0915490787
bingxinliu <bingxin.liu@wustl.edu>
2021-12-06 23:01:16 -0600

    init lab3

---
+++ lab3/producer/main.cpp
@@ -0,0 +1,13 @@
+
+
+/**
+ * @brief 
+ * initialize a producer object
+ * register appropriate event handlers
+ * run a reactive event loop to handle any signals, exceptions, or other events that may occur
+ * @return int 
+ */
+int main()
+{
+
+}
---
+++ lab3/producer/producer.hpp
@@ -0,0 +1,8 @@
+
+/**
+ * @brief 
+ * 1. interact with the user (stdin or stdout) to display status and receive commands
+ * 2. interact with the director object within director program processes
+ * 
+ */
+class producer{};
